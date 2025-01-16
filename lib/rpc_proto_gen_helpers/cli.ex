fedd6a08f54a67de5c728924378ceb8976327597
Hugo <hpjm37@gmail.com>
2025-01-16 12:12:32 +0000

    Revert "Use path for package components"
    This reverts commit 0f05581cb0bfb64a7465eeb8f89aba5f7305eb06.

--- lib/rpc_proto_gen_helpers/cli.ex
+++ lib/rpc_proto_gen_helpers/cli.ex
@@ -78,6 +78,7 @@ defmodule RPCProtoGenHelpers.CLI do
 
   defp build_service_metadata(%Google.Protobuf.FileDescriptorProto{
          name: file_path,
+         package: package,
          service: [
            %Google.Protobuf.ServiceDescriptorProto{name: service_name, method: methods}
          ],
@@ -85,7 +86,7 @@ defmodule RPCProtoGenHelpers.CLI do
            location: source_code_locations
          }
        }) do
-    package_components = file_path |> Path.dirname() |> Path.split()
+    package_components = package |> String.split(".")
     package_to_pascal = package_components |> Enum.map_join(".", &Recase.to_pascal/1)
     module_root = "#{package_to_pascal}.#{service_name}"
     file_name = file_path |> Path.basename() |> Path.rootname()
