bd6aee7bf767301dfb49c26447edc5ed11a43b74
Christian Gregg <christian@bissy.io>
2025-01-30 11:35:06 +0000

    Remove `RPCProtoGenHelpers.hello`

--- lib/rpc_proto_gen_helpers.ex
+++ lib/rpc_proto_gen_helpers.ex
@@ -1,18 +1,2 @@
 defmodule RPCProtoGenHelpers do
-  @moduledoc """
-  Documentation for `RPCProtoGenHelpers`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> RPCProtoGenHelpers.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/rpc_proto_gen_helpers_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RPCProtoGenHelpersTest do
-  use ExUnit.Case
-  doctest RPCProtoGenHelpers
-
-  test "greets the world" do
-    assert RPCProtoGenHelpers.hello() == :world
-  end
-end
